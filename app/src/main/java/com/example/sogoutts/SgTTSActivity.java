96b69888cf01d7a64aa624e218df722fd0015908
郜宁 <gaoning@gaoningdeMacBook-Pro.local>
2018-12-10 14:50:35 +0800

    增加版权信息

--- app/src/main/java/com/example/sogoutts/SgTTSActivity.java
+++ app/src/main/java/com/example/sogoutts/SgTTSActivity.java
@@ -1,3 +1,6 @@
+// Copyright 2018 Sogou Inc. All rights reserved. 
+// Use of this source code is governed by the Apache 2.0 
+// license that can be found in the LICENSE file. 
 package com.example.sogoutts;
 
 import android.app.Activity;
